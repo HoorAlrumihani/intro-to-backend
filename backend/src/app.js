55cb9e89e8c5183ef274d97d3994b36ebbc5f57b
Hoor <h66oor@gmail.com>
2026-01-31 15:41:55 +0300

    feat: add auth APIs (register, login, logout)

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express(); //create an express app
+app.use(express.json());
+
+import userRouter from '../routes/user.route.js';
+//import postRouter from './routers/post.route.js';
+app.use("/api/v1/users", userRouter);
+//app.use("/api/v1/posts", postRouter);
+
+
+
+export default app;
